b1d0e9c4a942fbb87f8adad77379d47c7febac18
firdausWahyu <farelW912006@gmail.com>
2025-12-17 22:44:18 +0700

    Struktur Program

--- src/gui/main/MainApp.java
+++ src/gui/main/MainApp.java
@@ -1,10 +1,13 @@
 package gui.main;
 
 import gui.dashboard.Class1;
+import gui.listdata.Class2;
+import gui.inputpage.Class3;
+import gui.history.Class4;
 
 //MainApp
 public class MainApp {
     public static void main(String[] args) {
-        new Class1();
+        new Class3();
     }
 }
