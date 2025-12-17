05c525f41075c9db3a4c253b61d0a4633d8dfd7f
firdausWahyu <farelW912006@gmail.com>
2025-12-17 23:01:19 +0700

    Struktur Program

---
+++ src/gui/inputpage/InputPage.java
@@ -0,0 +1,13 @@
+package gui.inputpage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class InputPage {
+
+    public InputPage() {
+
+    }
+}
--- src/gui/main/MainApp.java
+++ src/gui/main/MainApp.java
@@ -1,13 +1,13 @@
 package gui.main;
 
-import gui.dashboard.Class1;
-import gui.listdata.Class2;
-import gui.inputpage.Class3;
-import gui.history.Class4;
+import gui.inputpage.*;
+import gui.dashboard.*;
+import gui.history.*;
+import gui.listdata.*;
 
 //MainApp
 public class MainApp {
     public static void main(String[] args) {
-        new Class3();
+        new InputPage();
     }
 }
